92935571b744ae9f0802f0cc3ed4858777fac263
unknown <sainiaak@SAINIAAK8.asiapacific.hpqcorp.net>
2014-09-22 13:31:29 +0530

    Added files

---
+++ Candice/images/arrow-icon_files/deckNP_js.php
@@ -0,0 +1,19 @@
+// display the advertisement
+
+var advert = ''
+
+advert+='<a href="//adfarm.mediaplex.com/ad/nc/10680-205532-30163-0?mpt=1411112069">';
+advert+='<img class="ad" src="//adfarm.mediaplex.com/ad/nb/10680-205532-30163-0?mpt=1411112069" height="90" width="120" alt="Get more information about graduate programs at Parsons The New School for Design" border="0">';
+advert+='</a>';
+advert+='<p class="ads">';
+advert+='Get more information about graduate programs at <a href="//adfarm.mediaplex.com/ad/nc/10680-205532-30163-0?mpt=1411112069">Parsons The New School for Design</a>.';
+advert+='</p>';
+advert+='';
+advert+='';
+
+
+
+
+document.write(advert);
+
+
---
+++ Candice/js/parallax.js
@@ -0,0 +1,21 @@
+(function () {
+	
+	scrollConverter.activate();
+
+}());
+
+/*$(window).scroll(function (event) {
+    var y = window.pageXOffset;
+
+    if (y >=1950) {
+    	 firstAnimation();
+    }
+    y=window.pageXOffset;
+});
+var firstAnimation = function () {
+     $('#car').find('img').delay(1400).animate({
+            opacity: 1, 
+            left: -50
+        }, 'slow'
+    );
+};*/
